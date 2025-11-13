7913275742de8054f001309575d168c3528f5bf2
Aditya Gupta <adityagupta@Adityas-MacBook-Air-3.local>
2025-11-13 11:43:23 +0530

    assingnment

--- Multilevelinherit.java
+++ Multilevelinherit.java
@@ -1,26 +1,30 @@
 public class Multilevelinherit {
+
     public static void main(String[] args) {
-        Dog d = new Dog();
+        Puppy d = new Puppy();
         d.bark();
         d.eat();
-        d.weep()
+        d.weep();
     }
-    
 }
 
 class Animal {
-    void eat(){
+
+    void eat() {
         System.out.println("Animals can eat");
     }
 }
+
 class Dog extends Animal {
-    void bark(){
+
+    void bark() {
         System.out.println("Dog can bark");
     }
 }
-class Puppy extend Dog{
-    void weep(){
-        System.out.println("puppy can weep");
+
+class Puppy extends Dog {
+
+    void weep() {
+        System.out.println("Puppy can weep");
     }
 }
-
